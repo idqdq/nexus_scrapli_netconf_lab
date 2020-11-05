caa7c5e2f2172524fd774af9063970c2eaa4834c
IDQDD <nryzhkov@gmail.com>
2020-11-05 18:01:10 +0300

    xpath2xml supports slashes within elems

--- evpn_data_class.py
+++ evpn_data_class.py
@@ -53,6 +53,8 @@ class evpn_data:
                 tpl_tail)
 
 
+import re
+import xml.dom.minidom
 def xpath2xml(xpath, xmlns='', operation=None):
     #transforms xpath like string e.g. "/System/eps-items/epId-items/Ep-list/epId=1/nws-items/vni-items/Nw-list[]/vni=10444"
     #to xml-like sequence of elements tags: 
@@ -75,7 +77,15 @@ def xpath2xml(xpath, xmlns='', operation=None):
     #
     # it has optional parameter 'operation' that adds the string 'operation="value"' to the element marked with square brackets '[]'
     # for an nxos the value of the operation can be either "remove" or "replace"
+    #
+    # there is a problem with parsing elements that contain the '/' separator within like this tDn element has:
+    # /System/intf-items/svi-items/If-list/rtvrfMbr-items/tDn=/System/inst-items/Inst-list[name='{vrf_name}']
+    # the same case if you want to configure a physical interface like Eth103/1/20
+    # 
+    # as a workaround I mark all the slashes with additional one, then replace that doubleslashes with # sign
+    # then split xpath and unmark them back
 
+    xpath = re.sub(r'//', '#', xpath) # <-- replace doubleslashes with '#'
     pl = xpath.split('/')
     
     xmls = f'<{pl[1]} xmlns="{xmlns}">' if xmlns else f'<{pl[1]}>'
@@ -90,7 +100,8 @@ def xpath2xml(xpath, xmlns='', operation=None):
         for i in range(len(pl)):
             elem = pl[i]
             if "=" in elem:
-                elem,key = elem.split("=")
+                elem,key = elem.split("=", 1)
+                key = re.sub(r'#', '/', key) # <-- replace '#'
                 xmls += f'<{elem}>{key}</{elem}>'
                 break
             if "[]" in elem:
@@ -109,5 +120,8 @@ def xpath2xml(xpath, xmlns='', operation=None):
             return xmls + _xpath2xml(pl[(i+1)::]) + xmle #recursion
         else:
             return xmls + xmle
-     
+         
     return xmls + _xpath2xml(pl[2::]) + xmle
+
+def ppxml(xmlstr):
+    print(xml.dom.minidom.parseString(xmlstr).toprettyxml(indent="    "))
