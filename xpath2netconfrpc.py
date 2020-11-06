f69e12b4645521fd8214ecdd52f62dcf8e17ecac
IDQDD <nryzhkov@gmail.com>
2020-11-06 17:49:08 +0300

    polishing

--- xpath2netconfrpc.py
+++ xpath2netconfrpc.py
@@ -1,60 +1,6 @@
-from dataclasses import dataclass
-from ipaddress import IPv4Address, IPv4Interface, IPv4Network
-from tpl_evpn_xml import *
-
-@dataclass
-class evpn_data:
-    # mandatory data
-    vlan_id: int
-    vni: int
-    ip_address: str
-    # optional data (with defaults)
-    vlan_name: str = ''
-    mtu: int = 1500
-    description: str = 'anycast SVI'
-    vrf_name: str = 'Tenant-1'
-    mgroup: str = ''
-    supARP: bool = False
-
-    def __check_ip_int(self):
-        try :
-            IPv4Interface(self.ip_address)
-        except ValueError:
-            return False
-            
-        try: 
-            IPv4Network(self.ip_address)
-            return False
-        except:
-            return True
-
-    def __check_ip_mcast(self):
-        try: 
-            return IPv4Address(self.mgroup).is_multicast
-        except:
-            return False
-
-    def __post_init__(self):
-        if not self.vlan_name:
-            self.vlan_name = 'l2VNI-' + str(self.vni) 
-        if not self.__check_ip_int():
-            raise ValueError('Incorrect Interface IP address')
-        if self.mgroup:
-            if not self.__check_ip_mcast():
-                raise ValueError('Incorrect multicast group IP address')
-        self.supARP = 'enabled' if self.supARP else 'off'
-
-    def get_rpc(self):
-            return (tpl_head + 
-                tpl_bd_items.format(vlan_id=self.vlan_id, vlan_name=self.vlan_name, vni=self.vni) +
-                tpl_anycast_svi.format(vlan_id=self.vlan_id, mtu=self.mtu, description=self.description, vrf_name=self.vrf_name, ip_address=self.ip_address) + 
-                (tpl_nve_mcast_repl.format(vni=self.vni, mgroup=self.mgroup, supARP=self.supARP) if self.mgroup else tpl_nve_ingress_repl_bgp.format(vni=self.vni, supARP=self.supARP)) + 
-                tpl_bgp_evpn.format(vni=self.vni) + 
-                tpl_tail)
-
-
 import re
 import xml.dom.minidom
+
 def xpath2xml(xpath, xmlns='', operation=None):
     #transforms xpath like string e.g. "/System/eps-items/epId-items/Ep-list/epId=1/nws-items/vni-items/Nw-list[]/vni=10444"
     #to xml-like sequence of elements tags: 
@@ -95,8 +41,8 @@ def xpath2xml(xpath, xmlns='', operation=None):
         key = ''
         xmls = ''
         xmle = ''
-        operation_set = ("merge", "replace", "create", "delete", "remove")        
-
+        operation_set = ("remove", "replace")
+        
         for i in range(len(pl)):
             elem = pl[i]
             if "=" in elem:
@@ -104,6 +50,7 @@ def xpath2xml(xpath, xmlns='', operation=None):
                 key = re.sub(r'#', '/', key) # <-- replace '#'
                 xmls += f'<{elem}>{key}</{elem}>'
                 break
+            
             if "[]" in elem:
                 elem = elem[:-2]
                 if operation:
@@ -125,3 +72,7 @@ def xpath2xml(xpath, xmlns='', operation=None):
 
 def ppxml(xmlstr):
     print(xml.dom.minidom.parseString(xmlstr).toprettyxml(indent="    "))
+
+xpath = "/System/evpn-items/adminSt=enabled/bdevi-items/BDEvi-list/encap=vxlan-10010/rd=rd:unknown:0:0/rttp-items/RttP-list/type=export/ent-items/Rtt-Entry-list/rtt=route-target:unknown:0:0"
+
+ppxml(xpath2xml(xpath, xmlns='blabla'))
