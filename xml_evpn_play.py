208a21dcafb92a6ebb0a7dd892c284e337eb5dfd
IDQDD <nryzhkov@gmail.com>
2020-12-11 14:09:45 +0300

    step up

--- xml_evpn_play.py
+++ xml_evpn_play.py
@@ -14,28 +14,45 @@ for bd in bd_list:
     evpn["vlan"] = bd.findtext("fabEncap").split("-")[1]
     evpn["vni"] = bd.findtext("accEncap").split("-")[1]
     evpn_data.append(evpn)
-print(evpn_data)
 
-# enrich evpn_data with svi opts like mtu and vrf
+# enrich evpn_data with svi options: mtu, vrf, descr
+# ipv4 options: ip_address
+# hmm options: anycastGW
+# eps options (nve1): mcastGroup, suppressARP 
+# evpn options (rd/rt): rd, rtt_import, rtt_export
 for item in evpn_data:    
     vlan = f'vlan{item["vlan"]}'
+    vni = item["vni"]
+    vxlan = f'vxlan-{vni}'
+
     svi = et.xpath(f'.//svi-items/If-list[.//id="{vlan}"]')[0]
     item["mtu"] = svi.findtext("mtu")
     vrf = svi.xpath('.//rtvrfMbr-items/tDn/text()')[0]
-    item["vrf"] = re.split('[\[\]]', vrf)[1].split('=')[1].strip('\'')
-print(evpn_data)
+    item["vrf"] = re.split(r'[\[\]]', vrf)[1].split('=')[1].strip('\'')    
+    descr = svi.findtext("descr")
+    if descr:
+        item["descr"] = descr
 
-# enrich evpn_data with ipv4 data
-for item in evpn_data:    
-    vlan = f'vlan{item["vlan"]}'
     ipv4item = et.xpath(f'.//if-items/If-list[.//id="{vlan}"]')[0]
-    item["ip_address"] = ipv4item.xpath('.//addr-items/Addr-list/addr/text()')
+    ip_address = ipv4item.xpath('.//addr-items/Addr-list/addr/text()')
+    if ip_address:
+        item["ip_address"] = ip_address[0]
     
-print(evpn_data)
+    hmm = et.xpath(f'.//hmm-items/fwdinst-items/if-items/FwdIf-list[id="{vlan}" and mode="anycastGW"]')    
+    if hmm:
+        item["anycastGW"] = True
 
-# filter out l2vni's
-vni_list = et.xpath('.//Nw-list[.//associateVrfFlag="false"]')
+    eps = et.xpath(f'.//eps-items/epId-items/Ep-list[epId=1]/nws-items/vni-items/Nw-list[vni={vni} and associateVrfFlag="false"]')
+    if eps:
+        item["mcastGroup"] = eps[0].findtext("mcastGroup")
+        item["suppressARP"] = eps[0].findtext("suppressARP")
 
-for vni in vni_list:
-    print (f'vni: {vni.findtext("vni")}; mcast: {vni.findtext("mcastGroup")}')
+    evpn = et.xpath(f'.//evpn-items/bdevi-items/BDEvi-list[encap="{vxlan}"]')
+    if evpn:
+        item["rd"] = evpn[0].findtext("rd")
+        item["rtt_import"] = evpn[0].xpath('.//rttp-items/RttP-list[type="import"]/ent-items/RttEntry-list/rtt/text()')
+        item["rtt_export"] = evpn[0].xpath('.//rttp-items/RttP-list[type="export"]/ent-items/RttEntry-list/rtt/text()')
+        
+from pprint import pprint
+pprint(evpn_data)
 
