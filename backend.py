515f9627ed78422427c21bd5fadebd144ddd4961
IDQDD <nryzhkov@gmail.com>
2022-01-26 17:55:32 +0300

    xz2

--- backend.py
+++ backend.py
@@ -22,5 +22,6 @@ async def getEvpnAll():
         "evpnData": [
             {"vlan_id":"10","vni":"10010","vlan_name":"","svi_ip":"","svi_descr":"","mtu":"","vrf":"","mgroup":"","arpsup":True},
             {"vlan_id":"20","vni":"10020","vlan_name":"","svi_ip":"","svi_descr":"","mtu":"","vrf":"","mgroup":"","arpsup":False},
+            {"vlan_id":"30","vni":"10030","vlan_name":"Vlan30","svi_ip":"10.1.30.254/24","svi_descr":"","mtu":"1600","vrf":"Tenant-1","mgroup":"231.0.0.30","arpsup":True},
             ]
     }
